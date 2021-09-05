b972c446630213c95a7f14debcc280c54ec66d16
LikeMySty1e <markstime@mail.ru>
2021-09-05 15:39:05 +0300

    Fix for pseudo-elements and icons

--- FoodForStudent/FoodForStudent/settings.py
+++ FoodForStudent/FoodForStudent/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from secret import *
+from secret import SECRET_KEY
 from pathlib import Path
 import os
 
